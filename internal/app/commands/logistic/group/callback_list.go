83d2e5905ec1ee5d9be61946174d91d47d45b8fe
Roman Kovalev <rom@sirena2000.ru>
2021-10-20 01:28:53 +0300

    list callback error handle

--- internal/app/commands/logistic/group/callback_list.go
+++ internal/app/commands/logistic/group/callback_list.go
@@ -2,7 +2,6 @@ package group
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
@@ -13,16 +12,17 @@ type CallbackListData struct {
 	Offset uint64 `json:"offset"`
 }
 
-func (c *GroupCommander) CallbackList(callback *tgbotapi.CallbackQuery, callbackPath path.CallbackPath) {
+func (c *GroupCommander) CallbackList(callback *tgbotapi.CallbackQuery, callbackPath path.CallbackPath) error {
 	parsedData := CallbackListData{}
 	err := json.Unmarshal([]byte(callbackPath.CallbackData), &parsedData)
 	if err != nil {
 		log.Printf("failed to handle callback event:%+v", callbackPath.CallbackData)
 		msg := tgbotapi.NewMessage(
 			callback.Message.Chat.ID,
-			fmt.Sprintf("Parsed: %+v\n", parsedData),
+			"failed to handle callback message",
 		)
 		c.bot.Send(msg)
+		return err
 	}
-	c.ListOffset(callback.Message, parsedData.Offset)
+	return c.ListOffset(callback.Message, parsedData.Offset)
 }
